f1f0eb29b017524a96cf403437df199e92fdc5b1
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-29 21:47:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
   def name=(name)
-    @name
+    @Beyonce
   end
   def name
-    @name
+    @Beyonce
   end
   
   def job=(job)
-    @job
+    @singer
   end
   def job
-    @job
+    @singer
   end
 end
